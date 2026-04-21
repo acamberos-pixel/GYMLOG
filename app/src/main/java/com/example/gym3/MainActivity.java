fbb182cc288ed40a77fa00d96e5a665536531164
acamberos1 <camberos.athian1@gmail.com>
2026-04-20 21:12:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gym3/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.gym3;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+//import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gym3.databinding.ActivityMainBinding;
+
+import java.util.Locale;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+ActivityMainBinding binding;
+
+
+
+String mExercise = "";
+
+double numWeight = 0.0;
+
+int numReps =0;
+
+private static final String TAG = "DAC GYMLOG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+
+
+
+
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // makes some alert i think
+                getInformationFromDisplay();
+                updateDisplay();
+
+            }
+        });
+
+    }
+
+// pull value from screen input idk where yet
+    private void updateDisplay()
+    {
+        String currentInfo = binding.logDisplayTextView.getText().toString();
+        Log.d(TAG,"current info" +currentInfo);
+        String newDisplay = String.format(Locale.US,"Exercise: %s%nWeight:%.2f%nReps:%d%n:=-=-=-%n%s:",mExercise,numWeight,numReps, currentInfo);
+
+        binding.logDisplayTextView.setText(newDisplay);
+    }
+    private void getInformationFromDisplay()
+    {
+        mExercise = binding.exerciseInputEditText.getText().toString();
+try {
+    numWeight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+} catch (NumberFormatException e)
+
+{
+    Log.d(TAG , "error reading value from weight edit test") ;
+}
+
+try
+{
+    numReps = Integer.parseInt(binding.repInputEditText.getText().toString());
+}catch (NumberFormatException e)
+    {
+    Log.d(TAG , "error reading value from rep value edit test");
+    }
+    }
+}
