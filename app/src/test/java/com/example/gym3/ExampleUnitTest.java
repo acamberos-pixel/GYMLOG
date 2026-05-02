8d67387999f248888a588ddba91b2ec1fffffa66
acamberos1 <camberos.athian1@gmail.com>
2026-05-02 09:47:11 -0700

    tests added

--- app/src/test/java/com/example/gym3/ExampleUnitTest.java
+++ app/src/test/java/com/example/gym3/ExampleUnitTest.java
@@ -1,17 +1,32 @@
 package com.example.gym3;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testUserPassword() {
+        User user = new User("admin1", "admin1");
+        assertEquals("admin1", user.getPassword());
+        assertEquals("admin1", user.getUsername());
+    }
+
+    @Test
+    public void testUserIsNotAdminByDefault() {
+        User user = new User("testuser1", "testuser1");
+        assertFalse(user.isAdmin());
+    }
+
+    @Test
+    public void testUserAdminFlag() {
+        User user = new User("admin1", "admin1");
+        user.setAdmin(true);
+        assertTrue(user.isAdmin());
     }
 }
